dcab19351a3eddc84ba4e4254cf99fa888be1c3e
maged_refat <maged_refat@yahoo.com>
2014-12-14 23:06:30 +0200

    Vectorized with fixed IF statement

--- nw_vector_final.cc
+++ nw_vector_final.cc
@@ -1,26 +1,24 @@
-// ./nw-avx -a A -b ACGGAGTTACGGAGTT -m 0 -s 1 -g 1 -e 1
-// ./nw-avx -a ATAGAAGTAG -b TCAGTCAGTCAGAGCG -m 0 -s 1 -g 1 -e 1
-
 #include <immintrin.h>
 #include <unistd.h>
 #include <getopt.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #define BYTE_ALIGNMENT 32
 
-int padding = BYTE_ALIGNMENT / 4;   //padding = 8 or 4 
-
-int check(__m256 vector);
+int padding = BYTE_ALIGNMENT / 4;   //padding = 8 or 4 (vector size)
+int check2(__m256 vector1, __m256 vector2);
 void print(__m256 vector);
 __m256 shiftr7(__m256 vector);
 __m256 shiftl1(__m256 vector);
 
-//================ Main ===================
-//=========================================
+//========================= Main ========================
+//=======================================================
 
 int main(int argc, char * argv[] )
 {
-  printf("\n--------------------------------------\n");
+  printf("\n====================================================================\n");
+  clock_t start_t, end_t, total_t;
 
   char *a;
   char *b;
@@ -32,9 +30,12 @@ int main(int argc, char * argv[] )
   int n;
   int m;
   int mm;
+  int y;
+  int flag;
   float *score_matrix; // matrix filled with match and mismatchi costs
   float *HH;
   float *EE;
+
   __m256 x;
   __m256 E;
   __m256 T1;
@@ -46,7 +47,9 @@ int main(int argc, char * argv[] )
   __m256 H0;
   __m256 F0;
   __m256 F;
-  __m256 T2; 
+  __m256 T2;
+  __m256 init;
+  __m256 padding_mul_r;      // 8*rr
 
   if (argc == 1)
   {
@@ -76,19 +79,21 @@ int main(int argc, char * argv[] )
   qq= _mm256_set1_ps (q);
   rr= _mm256_set1_ps (r);
   qr= _mm256_add_ps(qq, rr);
+  padding_mul_r= _mm256_mul_ps( _mm256_set_ps(8, 8, 8, 8, 8, 8, 8, 8), rr);
+ 
   n= strlen(a); // database / columns
   m= strlen(b); // query / rows  
   mm=m;
 
 //  mm = ((m - 1) | (BYTE_ALIGNMENT - 1)) + 1;
-  mm = ((m - 1) | ( padding- 1)) + 1;
+  mm = ((m - 1) | ( padding - 1)) + 1;       //the new zize of the vector
 
   posix_memalign ((void **) &HH, BYTE_ALIGNMENT, mm * sizeof(float));
   posix_memalign ((void **) &EE, BYTE_ALIGNMENT, mm * sizeof(float));
   posix_memalign ((void **) &score_matrix, BYTE_ALIGNMENT,  n * mm * sizeof(float));
 
-//-----------------------------------------------------------
-  int l=0;                        // Filling the score matrix
+//---------------- Filling the score matrix ----------------
+  int l=0; 
   for (int i=0; i<n; i++)
   {
     char cc= a[i];
@@ -96,31 +101,45 @@ int main(int argc, char * argv[] )
     {
       char c=b[j];
       score_matrix[l] = (c==cc) ? match : mismatch;
-      l++;      
+      l++;
     }
   }
-//-----------------------------------------------------------
-
-  int y= mm/padding;
-  int flag=0; 
-
-  for(int i=0; i<(mm); i++)   //initializing HH and EE 
-  {
-    HH[i]= q + ( (i+1) * r );
-    EE[i]= q + ( q + (i+1)*r );
-  }
-
 //----------------------------------------------------------
 
-  for (int j=0; j<n;j++)
-  {
-    H0= _mm256_set_ps( 0, 0, 0, 0, 0, 0, 0,  q+ (j+1)*r  );
-    F0= _mm256_set_ps( 0, 0, 0, 0, 0, 0, 0, ( 2*q + (j+1)*r ) );
+//@@@@@@@@@@@@@@@@@@@@@@@@@@ start counting the time @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+start_t = clock();
+for(int counter=0; counter<1; counter++)
+{
+  y= mm/padding;
+  flag=0; 
 
-    if (j==0)
-      x= _mm256_setzero_ps();
-    else
-      x= _mm256_set_ps( 0, 0, 0, 0, 0, 0, 0, (q+ j*r) );
+//================== initializing HH and EE ==================
+  init= _mm256_set_ps( 8, 7, 6, 5, 4, 3, 2, 1);       // HH= q+ ( (i+1) * r )
+  init= _mm256_mul_ps( init, rr);
+
+  init= _mm256_add_ps( init, qq);
+  _mm256_store_ps(HH, init );
+  _mm256_store_ps(EE, _mm256_add_ps( init, qq) );     // EE= HH+ QQ
+
+  for(int i=1; i<y; i++)
+  {
+    init = _mm256_load_ps(&HH[ (i-1) * padding]); 
+    init = _mm256_add_ps( init, padding_mul_r );    // HH[i-1] + 8*rr
+    _mm256_store_ps(HH + (padding*i), init );
+
+    init = _mm256_load_ps(&EE[ (i-1) * padding]); 
+    init = _mm256_add_ps( init, padding_mul_r );    // EE[i-1] + 8*rr
+    _mm256_store_ps(EE + (padding*i), init );
+  }
+
+//=============================================================
+
+  for (int j=0; j<n;j++)
+  {  
+    x= ( (j==0) ? _mm256_setzero_ps() : _mm256_set_ps( 0, 0, 0, 0, 0, 0, 0, (q+ j*r) ) );
+    H0= _mm256_set_ps( 0, 0, 0, 0, 0, 0, 0,  q+ (j+1)*r  );
+    F0= _mm256_set_ps( 0, 0, 0, 0, 0, 0, 0, (2*q) + (j+1)*r );
+    F= _mm256_setzero_ps();
 
     for (int i=0;i<y;i++)
     { 
@@ -132,32 +151,34 @@ int main(int argc, char * argv[] )
 
       T1= shiftr7( H );
       H= _mm256_or_ps( shiftl1(H), x );
-      x=T1;               //pading for the H and E part
+      x= T1;                                     //pading for the H and E part
 
-      H=  _mm256_add_ps (H, score);
-      H= _mm256_min_ps ( H, E);  // min( H+score, E)
+      H= _mm256_add_ps (H, score);
+      H= _mm256_min_ps ( H, E);                  // min( H+score, E)
 
-//    print (H);                                /**/    open to print just H,E 
-//    _mm256_store_ps(HH +(padding*i), H );     /**/    open to print just H,E 
+//     print (H);                               //    open to print just H,E 
+//    _mm256_store_ps(HH +(padding*i), H );     //    open to print just H,E 
 
 //*********************************************
 
-      T2= _mm256_add_ps( _mm256_or_ps(shiftl1(H), H0), qr);
-//      if ( check( _mm256_sub_ps(H, T2) ))
-//      {
-        F=T2;
+      T2= _mm256_add_ps( _mm256_or_ps(shiftl1(H), H0), qr);    // the H+qr vector to be compared with F 
+      if (check2(H, T2) or check2(H, F) )
+      {
+        F=T2;  
         do
   	{
 	  F=_mm256_min_ps(F,T2);
           T2= _mm256_add_ps( _mm256_or_ps(shiftl1(F), F0), rr);
-	} while ( check( _mm256_sub_ps(F, T2) ));
+	} while ( check2(F, T2) );
 
-	H= _mm256_min_ps(H,F);
-/*      }
+        H= _mm256_min_ps(H,F);
+      }
 
       else 
-        F=_mm256_set_ps( ( 2*q + (j+1)*r ), 0, 0, 0, 0, 0, 0, 0);
-*/
+      {
+        F=_mm256_set_ps( (2*q) + (j+1)*r, 0, 0, 0, 0, 0, 0, 0);
+      }
+
       H0=shiftr7(H);
       F0=shiftr7(F);
       print (H);
@@ -168,6 +189,11 @@ int main(int argc, char * argv[] )
     }
     printf("\n");
   }
+}
+  end_t = clock();
+  total_t = ((double)(end_t - start_t)) / CLOCKS_PER_SEC;
+//  printf("Total time taken by CPU: %.4f\n", (double)total_t  );
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
   free(score_matrix);
   free(HH);
@@ -176,12 +202,11 @@ int main(int argc, char * argv[] )
 }
 
 //==========================================
-//---------------- check -------------------
-int check(__m256 vector)
+//---------------- check2 -------------------
+int check2(__m256 vector1, __m256 vector2)
 {
-  __m256 vcmp = _mm256_cmp_ps(_mm256_setzero_ps(), vector, _CMP_LT_OQ);
+  __m256 vcmp = _mm256_cmp_ps(vector2, vector1, _CMP_LT_OQ);
   int cmp = _mm256_movemask_ps(vcmp);
-
   return ((cmp>0) ? 1 : 0) ;
 }
 
@@ -203,7 +228,7 @@ __m256 shiftr7(__m256 vector)
   return ans;
 }
 
-//-----------------------------------------
+//--------------- print --------------------
 void print ( __m256 vector)
 {
   float * temp;
